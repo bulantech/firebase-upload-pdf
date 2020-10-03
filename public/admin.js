944398c2dac437371632d500c21415af532ae235
bulantech <bulantech@gmail.com>
2020-10-03 17:43:49 +0700

    add delete

--- public/admin.js
+++ public/admin.js
@@ -1,12 +1,13 @@
 var dTable = null
 var db = firebase.firestore();
 var dataAll = []
+var file = null
 
 $( document ).ready(function() {
   dTable = $('#myTable').DataTable( {
     "columnDefs": [
       {
-        "targets": [ 0 ],
+        "targets": [ 0,1 ],
         "visible": false,
         "searchable": false
       },
@@ -16,7 +17,7 @@ $( document ).ready(function() {
   $('#myTable tbody').on( 'click', 'tr', function () {
     console.log('#myTable tbody..')
     if ( $(this).hasClass('selected') ) {
-        $(this).removeClass('selected');
+      // $(this).removeClass('selected');
     }
     else {
       dTable.$('tr.selected').removeClass('selected');
@@ -34,61 +35,47 @@ $( document ).ready(function() {
     toggleSignIn()
   });
 
-  // $( "#addฺNow" ).click(function( event ) {    
-  //   $('#addModal').modal('show');
-  // });
-
-  $( "#addModal" ).find('button[name="add"]').click(function( event ) {    
-    console.log('add..')
-    const topic = 'เรื่องทดสอบ' + Date.now()//doc.data().topic
-    const status = 'เผยแพร่'//doc.data().status
-    const type = 'จัดซื้อจัดจ้าง'//doc.data().type
-    const publishDate = firebase.firestore.Timestamp.fromDate(new Date()); //doc.data().publishDate.toDate().toLocaleDateString('th-TH', options).split('ที่')[1]
-    const fileName = 'จัดซื้อจัดจ้าง.pdf'//doc.data().filePath
-    const filePath = 'https://firebasestorage.googleapis.com/v0/b/test-702f6.appspot.com/o/upload%2FMCD-ST%20Liberty%20SW%20License%20Agreement%20V2.pdf?alt=media&token=285b8c33-4f30-49f1-abd7-59ec80628aaf'//doc.data().fileName
-    
-    const data = { topic, status, type, publishDate, fileName, filePath }
-
-    // Add a new document with a generated id.
-    db.collection("upload").add(data)
-    .then(function(docRef) {
-      console.log("Document written with ID: ", docRef.id);
-      const _id = docRef.id
-      const file = '<a target="_blank" rel="noopener noreferrer" href="' +filePath+ '">' +fileName+ '</a>'
-
-      const btnEdit = '<button type="button" class="btn btn-primary btn-sm mx-1">แก้ไข</button>'
-      const btnDelete = '<button type="button" class="btn btn-danger btn-sm mx-1" onClick="showDelete(\'' + _id + '\')">ลบ</button>'
-      const editDelete = '<span class="btn-group">' + btnEdit + btnDelete + '</span>'
-
-      const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-      const publishDate1 = publishDate.toDate().toLocaleDateString('th-TH', options).split('ที่')[1]
-
-      dTable.row.add( [_id, topic, type, status, file, publishDate1, editDelete] ).draw();
-
-      $('#addModal').modal('hide');
+  $( "#addฺNow" ).click(function( event ) {  
+    $("#addModal").find('button[name="add"]').removeClass('d-none')
+    $("#addModal").find('button[name="edit"]').addClass('d-none')  
+    $("#addModal").find('.modal-title').text('เพิ่มประกาศ')
+    $("#addModal").find('input').each(function () {
+      $(this).val('');
     })
-    .catch(function(error) {
-      console.error("Error adding document: ", error);
-      $('#addModal').modal('hide');
-    });
+    $('#addModal').modal('show');
+  });
+
+  $( "#addForm" ).submit(function( event ) {
+    const title = $("#addModal").find('.modal-title').text()
+    console.log('title', title)
+    if(title === 'เพิ่มประกาศ') {
+      addData()
+    } else {
+      editData()
+    }
+    event.preventDefault();  
   });
 
   $( "#delete" ).click(function( event ) {  
-    const id = dTable.row('.selected').data()[0]
-    console.log('delete..', id)  
-        
-    db.collection("upload").doc(id).delete().then(function() {
-      console.log("Document successfully deleted!");
-      dTable.row('.selected').remove().draw( false );
-    }).catch(function(error) {
-      console.error("Error removing document: ", error);
-    });
+    deleteData()
+  });
 
-    $('#deleteModal').modal('hide');
+  $('.datepicker').datepicker({
+    language: 'th',
+    format: 'dd-mm-yyyy',
+  });
+
+  // Add the following code if you want the name of the file appear on select
+  $(".custom-file-input").on("change", function(event) {
+    // console.log('event =>', event)
+    file = event.target.files[0];
+    var fileName = $(this).val().split("\\").pop();
+    $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
   });
 
   initApp()
-});
+
+}); // end $( document ).ready(function() {
 
 initApp = () => {   
   firebase.auth().onAuthStateChanged(function(user) {
@@ -156,25 +143,160 @@ getDatabase = () => {
       const type = doc.data().type
       const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
       const publishDate = doc.data().publishDate.toDate().toLocaleDateString('th-TH', options).split('ที่')[1]
-      const path = doc.data().filePath
+      const fileUrl = doc.data().fileUrl
+      const filePath = doc.data().filePath
       const fileName = doc.data().fileName
-      const file = '<a target="_blank" rel="noopener noreferrer" href="' +path+ '">' +fileName+ '</a>'
+      const file = '<a target="_blank" rel="noopener noreferrer" href="' +fileUrl+ '">' +fileName+ '</a>'
       const _id = doc.id
       const data = {...doc.data(), _id: doc.id}
 
-      const btnEdit = '<button type="button" class="btn btn-primary btn-sm mx-1">แก้ไข</button>'
+      const btnEdit = '<button type="button" class="btn btn-primary btn-sm mx-1" onClick="showEdit(\'' + _id+ '\')">แก้ไข</button>'
       const btnDelete = '<button type="button" class="btn btn-danger btn-sm mx-1" onClick="showDelete(\'' + topic+ '\')">ลบ</button>'
       const editDelete = '<span class="btn-group">' + btnEdit + btnDelete + '</span>'
 
       dataAll.push(data)
       console.log('dataAll =>', dataAll)
-      dTable.row.add( [_id, topic, type, status, file, publishDate, editDelete] ).draw();
+      dTable.row.add( [_id, filePath, topic, type, file, publishDate, editDelete] ).draw();
     });
   });
 }
 
 showDelete = (topic) => {
+  console.log('showDelete..')
   // const result = dataAll.find( ({ _id }) => _id === id );
+  // topic = dTable.row('.selected').data()[2]
   $('#deleteModal').find('.modal-body').text(topic)
   $('#deleteModal').modal('show');
+}
+
+showEdit = (id) => {
+  // const result = dataAll.find( ({ _id }) => _id === id );
+  $('#addModal').data('id', id)
+  $("#addModal").find('button[name="add"]').addClass('d-none')
+  $("#addModal").find('button[name="edit"]').removeClass('d-none')  
+  $("#addModal").find('.modal-title').text('แก้ไขประกาศ')
+  $('#addModal').modal('show');
+}
+
+// add
+addData = () => {
+// $( "#addModal" ).find('button[name="add"]').click(function( event ) {      
+  console.log('add..')
+  const storageRef = firebase.storage().ref('upload');
+  const metadata = {
+    'contentType': file.type
+  };
+  const yyyy = new Date().getFullYear()
+  const mm = new Date().getMonth()
+  const dd = new Date().getDate()
+  const filePath = yyyy+'/'+mm+'/'+dd+'/'+file.name
+
+  storageRef.child(filePath).put(file, metadata).then(function(snapshot) {
+    // console.log('Uploaded', snapshot.totalBytes, 'bytes.');
+    // console.log('File metadata:', snapshot.metadata);
+    snapshot.ref.getDownloadURL().then(function(url) {
+      // console.log('File available at', url);
+      const topic = $("#addModal").find('input[name="topic"]').val()
+      // const status = 'เผยแพร่'//doc.data().status
+      const type = $("#addModal").find('option:selected').text();
+      const date = $('.datepicker').datepicker("getDate") //$(".datepickerr").data('datepicker').getFormattedDate('yyyy-mm-dd') //$("#addModal").find('input[name="date"]').data('date')
+      const publishDate = firebase.firestore.Timestamp.fromDate(new Date(date));
+      const insertDate = firebase.firestore.Timestamp.fromDate(new Date());       
+      const fileName = file.name
+      const fileUrl = url        
+      const data = { topic, status, type, publishDate, fileName, filePath, fileUrl, insertDate }
+      // console.log('data =>',date , data);
+
+      // Add a new document with a generated id.
+      db.collection("upload").add(data)
+      .then(function(docRef) {
+        console.log("Document written with ID: ", docRef.id);
+        const _id = docRef.id
+        const file = '<a target="_blank" rel="noopener noreferrer" href="' +fileUrl+ '">' +fileName+ '</a>'
+
+        const btnEdit = '<button type="button" class="btn btn-primary btn-sm mx-1" onClick="showEdit(\'' + _id+ '\')">แก้ไข</button>'
+        const btnDelete = '<button type="button" class="btn btn-danger btn-sm mx-1" onClick="showDelete(\'' + topic + '\')">ลบ</button>'
+        const editDelete = '<span class="btn-group">' + btnEdit + btnDelete + '</span>'
+
+        const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+        const publishDate1 = publishDate.toDate().toLocaleDateString('th-TH', options).split('ที่')[1]
+
+        dTable.row.add( [_id, filePath, topic, type, file, publishDate1, editDelete] ).draw();
+
+        $('#addModal').modal('hide');
+      })
+      .catch(function(error) {
+        console.error("Error adding document: ", error);
+        $('#addModal').modal('hide');
+      });
+    });
+
+  }).catch(function(error) {
+    console.error('Upload failed:', error);
+  });
+
+  // });
+}
+
+// edit
+editData = () => {
+// $("#addModal").find('button[name="edit"]').click(function( event ) {
+  const id = $("#addModal").data('id')
+  // const result = dataAll.find( ({ _id }) => _id === id )
+  db.collection("upload").doc(id).update({
+    capital: true
+  })
+  .then(function() {
+    console.log("Document successfully updated!");
+
+    const topic = 'doc.data().topic'
+    const status = 'doc.data().status'
+    // if(status!='publish') return
+    const type = 'doc.data().type'
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    const publishDate = new Date().toLocaleDateString('th-TH', options).split('ที่')[1]
+    const path = 'doc.data().filePath'
+    const fileName = 'doc.data().fileName'
+    const file = '<a target="_blank" rel="noopener noreferrer" href="' +path+ '">' +fileName+ '</a>'
+    const _id = id
+    // const data = {...doc.data(), _id: doc.id}
+    const btnEdit = '<button type="button" class="btn btn-primary btn-sm mx-1" onClick="showEdit(\'' + _id+ '\')">แก้ไข</button>'
+    const btnDelete = '<button type="button" class="btn btn-danger btn-sm mx-1" onClick="showDelete(\'' + topic+ '\')">ลบ</button>'
+    const editDelete = '<span class="btn-group">' + btnEdit + btnDelete + '</span>'
+
+    dTable.row('.selected').data([_id, topic, type, file, publishDate, editDelete]).draw();
+    $('#addModal').modal('hide');
+  })
+  .catch(function(error) {
+    // The document probably doesn't exist.
+    console.error("Error updating document: ", error);
+    $('#addModal').modal('hide');
+  });
+// });
+}
+
+// delete
+deleteData = () => {
+  const id = dTable.row('.selected').data()[0]
+  const filePath = dTable.row('.selected').data()[1]
+  console.log('delete..', id, filePath)  
+      
+  db.collection("upload").doc(id).delete().then(function() {
+    console.log("Document successfully deleted!");
+    dTable.row('.selected').remove().draw( false );
+    $('#deleteModal').modal('hide');
+  }).catch(function(error) {
+    console.error("Error removing document: ", error);
+    $('#deleteModal').modal('hide');
+  });
+
+  // Create a reference to the file to delete
+  const storageRef = firebase.storage().ref('upload');
+  const desertRef = storageRef.child(filePath);
+  // Delete the file
+  desertRef.delete().then(function() {
+    console.log("File deleted successfully");
+  }).catch(function(error) {
+    console.log("Uh-oh, an error occurred!");
+  });
 }
--- public/view.js
+++ public/view.js
@@ -5,23 +5,21 @@ db.collection("upload").orderBy("publishDate", "desc").get().then((querySnapshot
     console.log(`${doc.id} => ${doc.data()}`, doc.data(), doc.data().insertDate.toDate() );
     
     const status = doc.data().status
-    if(status!='publish') return
+    // if(status!='publish') return
 
     const type = doc.data().type
     const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
     const publishDate = doc.data().publishDate.toDate().toLocaleDateString('th-TH', options).split('ที่')[1]
-    const path = doc.data().filePath
-    const btn = '<button type="button" class="btn btn-sm btn-outline-primary ml-2" onclick="window.open(\''+ path +'\')" >ดูเอกสาร</button>'
+    const fileUrl = doc.data().fileUrl
+    const btn = '<button type="button" class="btn btn-sm btn-outline-primary ml-2" onclick="window.open(\''+ fileUrl +'\')" >ดูเอกสาร</button>'
     // const btn = '<span class="badge badge-primary ml-2">ดูเอกสาร</span>'
     const str = '<p class="mb-0">' + doc.data().topic + ' ณ วันที่ ' + publishDate + btn + '</p>'
     switch(type) {
-      case 'news':
+      case 'ข่าวสาร':
         $('#not-news').addClass('d-none')
         $('#nav-news').append(str)
-        $('#nav-news').append(str)
-        $('#nav-news').append(str)
         break
-      case 'procurement':
+      case 'จัดซื้อจัดจ้าง':
         $('#not-procurement').addClass('d-none')
         $('#nav-procurement').append(str)
         break
